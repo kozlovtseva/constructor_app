c92d896c246e17bcc9486085fb843e6842f82a93
Kozlovtseva <alina.lm@mail.ru>
2019-06-15 17:16:48 +0300

    setting up redux

--- App.js
+++ App.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/store';
+import Main from './components/MainComponent';
+
+const store = ConfigureStore();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    return (
+        <Provider store={store}>
+            <Main />
+        </Provider>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
